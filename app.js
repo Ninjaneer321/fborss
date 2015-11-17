36a99182b4deb559553d34c8f88cefcf0be41f24
Bill Dollins <bill@geomusings.com>
2015-11-16 19:51:31 -0500

    port

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
+var http = require('http');
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT); 
-app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP);
-//app.listen();
-var server = app.listen(app.port, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
